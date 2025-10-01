747deb012ce8732dc2a6deefc296dd996c3151a9
Cicada1997 <francis.backstrom@gmail.com>
2025-10-02 01:08:42 +0200

    a

---
+++ target/debug/deps/spel1-1b1d3b20cb24ae29.d
@@ -0,0 +1,18 @@
+/home/cicada/dev/spel1/target/debug/deps/spel1-1b1d3b20cb24ae29.d: src/main.rs src/components.rs src/components/player.rs src/components/enemy.rs src/components/health.rs src/components/position.rs src/components/direction.rs src/components/visual.rs src/systems.rs src/systems/rendering.rs src/systems/entity_movement.rs src/game_events.rs src/game_event_handler.rs src/game.rs
+
+/home/cicada/dev/spel1/target/debug/deps/spel1-1b1d3b20cb24ae29: src/main.rs src/components.rs src/components/player.rs src/components/enemy.rs src/components/health.rs src/components/position.rs src/components/direction.rs src/components/visual.rs src/systems.rs src/systems/rendering.rs src/systems/entity_movement.rs src/game_events.rs src/game_event_handler.rs src/game.rs
+
+src/main.rs:
+src/components.rs:
+src/components/player.rs:
+src/components/enemy.rs:
+src/components/health.rs:
+src/components/position.rs:
+src/components/direction.rs:
+src/components/visual.rs:
+src/systems.rs:
+src/systems/rendering.rs:
+src/systems/entity_movement.rs:
+src/game_events.rs:
+src/game_event_handler.rs:
+src/game.rs:
